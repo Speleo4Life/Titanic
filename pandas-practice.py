904791893e702337cca7a35813eeb40f102a09a9
Ray MacNeil <raymond.macneil@mail.utoronto.ca>
2021-12-28 18:19:51 -0800

    Pandas practice.

---
+++ pandas-practice.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+obj = pd.Series([4,7,-5,])
